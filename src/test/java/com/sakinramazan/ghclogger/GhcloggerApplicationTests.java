51ffbbfce52372ce2ac6c0e87fc43b99c774726e
Ramazan Sakin <ramazansakin63@gmail.com>
2021-03-14 13:42:47 +0300

    project base started via spring init

---
+++ src/test/java/com/sakinramazan/ghclogger/GhcloggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sakinramazan.ghclogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhcloggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
