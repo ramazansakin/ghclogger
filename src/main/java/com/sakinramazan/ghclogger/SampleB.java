5fd871d273c1b801c05432c071928c2d9dad82b1
Ramazan Sakin <ramazansakin63@gmail.com>
2021-06-21 08:38:24 +0300

    feat: sampleB feature impl

---
+++ src/main/java/com/sakinramazan/ghclogger/SampleB.java
@@ -0,0 +1,21 @@
+package com.sakinramazan.ghclogger;
+
+import java.util.Objects;
+
+public class SampleB {
+
+	private String B;
+
+	public String getB() {
+		return B;
+	}
+
+	public void setB(String b) {
+		B = b;
+	}
+
+	@Override
+	public String toString() {
+		return "SampleB{" + "B='" + B + '\'' + '}';
+	}
+}
