4a5d23ab371c3f1ab9475314beae1fc5bd5a09cd
Ramazan Sakin <ramazansakin63@gmail.com>
2021-09-18 07:46:30 +0300

    Test Logs

--- src/main/java/com/sakinramazan/ghclogger/GhcloggerApplication.java
+++ src/main/java/com/sakinramazan/ghclogger/GhcloggerApplication.java
@@ -1,13 +1,24 @@
 package com.sakinramazan.ghclogger;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GhcloggerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GhcloggerApplication.class, args);
-	}
+    private static final Logger logger = LogManager.getLogger(GhcloggerApplication.class);
+
+    public static void main(String[] args) {
+
+        logger.info("Sample Messages with Different types to test logs");
+        logger.info("Sample Info message");
+        logger.error("Sample Error message");
+        logger.debug("Sample Debug message");
+        logger.warn("Sample Warning message");
+
+        SpringApplication.run(GhcloggerApplication.class, args);
+    }
 
 }
